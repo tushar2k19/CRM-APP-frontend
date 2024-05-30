5b365ddb456f42396ab8fa06855081905947ee85
Tushar <tushrrrocks@gmail.com>
2024-05-30 17:40:53 +0530

    revert this

--- src/backend/axios/index1.js
+++ src/backend/axios/index1.js
@@ -35,11 +35,8 @@ securedAxiosInstance.interceptors.request.use(config => {
 
 securedAxiosInstance.interceptors.response.use(null, error => {
   if (error.response && error.response.config && error.response.status === 401) {
-    console.log("inside the response error");
     return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
       .then(response => {
-        console.log("inside the response error (then)");
-
         localStorage.csrf = response.data.csrf
         localStorage.signedIn = true
 
@@ -47,7 +44,6 @@ securedAxiosInstance.interceptors.response.use(null, error => {
         retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
         return plainAxiosInstance.request(retryConfig)
       }).catch(error => {
-        console.log("inside the response error (catch)");
         delete localStorage.csrf
         delete localStorage.signedIn
 
@@ -55,7 +51,6 @@ securedAxiosInstance.interceptors.response.use(null, error => {
         return Promise.reject(error)
       })
   } else {
-    console.log('outside the response error')
     return Promise.reject(error)
   }
 })
